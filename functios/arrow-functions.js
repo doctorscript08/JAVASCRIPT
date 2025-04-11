189662a11f4ec0cff9269b1911a7c46154a38c72
doctorscript <kelsondesousa99@gmail.com>
2025-04-11 21:17:38 +0100

    Recapitulando functions e aprendendo arrow functions

---
+++ functios/arrow-functions.js
@@ -0,0 +1,11 @@
+//Função tradicional
+function soma(a, b) {
+    return a + b
+}
+
+//Arrow function
+const soma = (a, b) => {
+    return a + b
+}
+
+const soma = (a, b) => a + b
---
+++ functios/functions.js
@@ -0,0 +1,13 @@
+//Funções são blocos de código executados quando chamados ou em decorrência de alguma coisa
+
+function soma(valor1, valor2) {
+    return valor1 + valor2
+}
+
+console.log(soma(10, 10))
+
+function conversao_moeda(kwanza, dolar) {
+    return dolar * kwanza
+}
+
+console.log(conversao_moeda(912, 100))
